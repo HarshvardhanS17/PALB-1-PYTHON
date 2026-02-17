ba689a2c7ff5fc31fcfde5528d0c803e10a9763a
Harshvardhan Sikroriya <sik.harshvardhan@gmail.com>
2026-02-17 09:33:47 +0530

    Add function to minimize height difference in array
    Implement a solution to minimize height difference in an array.

---
+++ sheet2/minimize the height.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getMinDiff(self, arr, k):
+        n = len(arr)
+        arr.sort()
+
+        ans = arr[-1] - arr[0]  # initial difference
+
+        small = arr[0] + k
+        big = arr[-1] - k
+
+        if small > big:
+            small, big = big, small
+
+        for i in range(1, n - 1):
+
+            subtract = arr[i] - k
+            add = arr[i] + k
+
+            if subtract >= small or add <= big:
+                continue
+
+            if big - subtract <= add - small:
+                small = subtract
+            else:
+                big = add
+
+        return min(ans, big - small)
